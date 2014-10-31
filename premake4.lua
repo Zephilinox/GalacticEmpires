cd779e908f352e72c6fbbb580f9afc1d9f2c0ae5
Zephilinox <Zephilinox@hotmail.co.uk>
2014-10-31 08:30:50 +0000

    premake working

--- premake4.lua
+++ premake4.lua
@@ -1,26 +1,31 @@
 -- A solution contains projects, and defines the available configurations
 solution "GalacticEmpires"
 	configurations {"Debug", "Release"}
-	location "build"
+	location ("builds/".._ACTION)
 	
 	-- A project defines one build target
 	project "GalacticEmpires"
+		location ("builds/".._ACTION)
+		objdir ("builds/".._ACTION.."/obj")
 		language "C++"
 		files {"main.cpp", "include/**.hpp", "src/**.cpp"}
 		includedirs {"include", "extlibs/SFML/include", "extlibs/SFGUI/include", "extlibs/LuaBridge/include"}
 		libdirs {"extlibs/SFML/lib", "extlibs/SFGUI/lib"}
 		defines {"SFML_STATIC", "SFGUI_STATIC"}
-		links {"liblua", "sfgui"}
+		links {"lua", "sfgui-s"}
+		linkoptions {"-static", "-static-libgcc", "-static-libstdc++"}
 		buildoptions "-std=c++11"
 		
 		configuration "Debug"
 			kind "ConsoleApp"
+			targetdir ("builds/".._ACTION.."/bin/Debug/")
 			defines {"DEBUG"}
 			flags {"Symbols"}
 			links {"sfml-graphics-s-d", "sfml-window-s-d", "sfml-system-s-d"}
  
 		configuration "Release"
 			kind "WindowedApp"
+			targetdir ("builds/".._ACTION.."/bin/Release/")
 			defines {"NDEBUG"}
 			flags {"Optimize"}
 			links {"sfml-graphics-s", "sfml-window-s", "sfml-system-s"}
