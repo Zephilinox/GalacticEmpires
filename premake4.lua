dbdcc67f27ba9395345c4a732d7d8b01a76bb5e7
Zephilinox <Zephilinox@hotmail.co.uk>
2014-11-02 18:05:47 +0000

    tabs and spaces

--- premake4.lua
+++ premake4.lua
@@ -6,31 +6,31 @@ local build_tool = ""
 --determine what is being used to compile and if that is supported
 --environment is used to determine which subfolder of extlibs to use when looking for libraries
 if system == "windows" then
-	if _ACTION == "codeblocks" or _ACTION == "codelite" then
-		environment = "mingw"
-	elseif _ACTION == "vs2010" or _ACTION == "vs2008" or _ACTION == "vs2005" or _ACTION == "vs2003" or _ACTION == "vs20002" then
-		environment = "msvc"
-	else
-		print("Using '".._ACTION.."' on '"..system.."' is not supported")
-		os.exit();
-	end	
+    if _ACTION == "codeblocks" or _ACTION == "codelite" then
+        environment = "mingw"
+    elseif _ACTION == "vs2010" or _ACTION == "vs2008" or _ACTION == "vs2005" or _ACTION == "vs2003" or _ACTION == "vs20002" then
+        environment = "msvc"
+    else
+        print("Using '".._ACTION.."' on '"..system.."' is not supported")
+        os.exit();
+    end    
 elseif system == "macosx" then
-	if _ACTION == "xcode3" then
-		environment = "mac"
-	else
-		print("Using '".._ACTION.."' on '"..system.."' is not supported")
-		os.exit();
-	end
+    if _ACTION == "xcode3" then
+        environment = "mac"
+    else
+        print("Using '".._ACTION.."' on '"..system.."' is not supported")
+        os.exit();
+    end
 elseif system == "linux" or system == "bsd" or system == "solaris" then
-	if _ACTION == "codeblocks" or _ACTION == "codelite" or _ACTION == "gmake" then
-		environment = "linux"
-	else
-		print("Using '".._ACTION.."' on '"..system.."' is not supported")
-		os.exit();
-	end
+    if _ACTION == "codeblocks" or _ACTION == "codelite" or _ACTION == "gmake" then
+        environment = "linux"
+    else
+        print("Using '".._ACTION.."' on '"..system.."' is not supported")
+        os.exit();
+    end
 else
-	print("Unknown system '"..system.."'")
-	os.exit();
+    print("Unknown system '"..system.."'")
+    os.exit();
 end
 
 print("system = "..system)
@@ -66,59 +66,59 @@ os.copydir("data", "builds/".._ACTION.."/bin/Release/data")
 
 --For windows we will want to copy over the .dll's of libraries to the dll folder, e.g. builds/codeblocks/dll
 if system == "windows" then
-	print("copying DLL files")
-	os.mkdir("builds/".._ACTION.."/dll/")
-	
-	os.copyfile("extlibs/"..environment.."/SFML/bin/openal32.dll", "builds/".._ACTION.."/dll/openal32.dll")
-	os.copyfile("extlibs/"..environment.."/SFML/bin/libsndfile-1.dll", "builds/".._ACTION.."/dll/libsndfile-1.dll")
-	
-	os.copydir("builds/".._ACTION.."/dll", "builds/".._ACTION.."/bin/Debug/") --place alongside executable
-	os.copydir("builds/".._ACTION.."/dll", "builds/".._ACTION.."/bin/Release/")
+    print("copying DLL files")
+    os.mkdir("builds/".._ACTION.."/dll/")
+    
+    os.copyfile("extlibs/"..environment.."/SFML/bin/openal32.dll", "builds/".._ACTION.."/dll/openal32.dll")
+    os.copyfile("extlibs/"..environment.."/SFML/bin/libsndfile-1.dll", "builds/".._ACTION.."/dll/libsndfile-1.dll")
+    
+    os.copydir("builds/".._ACTION.."/dll", "builds/".._ACTION.."/bin/Debug/") --place alongside executable
+    os.copydir("builds/".._ACTION.."/dll", "builds/".._ACTION.."/bin/Release/")
 end
 
 solution("GalacticEmpires")
-	configurations 	{"Debug", "Release"}
-	location 		("builds/".._ACTION)
-	
-	project "GalacticEmpires"
-		language 		("C++")
-		location 		("builds/".._ACTION)
-		objdir 			("builds/".._ACTION.."/obj")
-		
-		files 			{"main.cpp", "include/**.hpp", "src/**.cpp"}
-		buildoptions 	("-std=c++11 -Wall -Wextra -Woverloaded-virtual")
-		
-		includedirs 	{include_dirs_header_only}
-		libdirs			(libs)
-		
-		if system == "windows" then
-			linkoptions {"-static-libgcc", "-static-libstdc++"}
-			includedirs	(include_dirs_libs)
-			libdirs		("builds/".._ACTION.."/dll")
-		end
-		
-		configuration("Debug")
-			kind 			("ConsoleApp")
-			defines 		{"DEBUG"}
-			flags 			{"Symbols", "ExtraWarnings"}
-			targetdir 		("builds/".._ACTION.."/bin/Debug/")
-			
-			if system == "windows" then
-				defines 	{"SFML_STATIC", "SFGUI_STATIC"}
-				links 		{"lua52", "sfgui-s", "thor-s-d", "sfml-graphics-s-d", "sfml-window-s-d", "sfml-system-s-d"}
-			else 
-				links 		{"lua5.2", "sfgui", "thor", "sfml-graphics", "sfml-window", "sfml-system"}
-			end
+    configurations  {"Debug", "Release"}
+    location        ("builds/".._ACTION)
+    
+    project "GalacticEmpires"
+        language        ("C++")
+        location        ("builds/".._ACTION)
+        objdir          ("builds/".._ACTION.."/obj")
+        
+        files           {"main.cpp", "include/**.hpp", "src/**.cpp"}
+        buildoptions    ("-std=c++11 -Wall -Wextra -Woverloaded-virtual")
+        
+        includedirs     {include_dirs_header_only}
+        libdirs         (libs)
+        
+        if system == "windows" then
+            linkoptions {"-static-libgcc", "-static-libstdc++"}
+            includedirs (include_dirs_libs)
+            libdirs     ("builds/".._ACTION.."/dll")
+        end
+        
+        configuration("Debug")
+            kind            ("ConsoleApp")
+            defines         {"DEBUG"}
+            flags           {"Symbols", "ExtraWarnings"}
+            targetdir       ("builds/".._ACTION.."/bin/Debug/")
+            
+            if system == "windows" then
+                defines     {"SFML_STATIC", "SFGUI_STATIC"}
+                links       {"lua52", "sfgui-s", "thor-s-d", "sfml-graphics-s-d", "sfml-window-s-d", "sfml-system-s-d"}
+            else 
+                links       {"lua5.2", "sfgui", "thor", "sfml-graphics", "sfml-window", "sfml-system"}
+            end
 
-		configuration("Release")
-			kind 			("WindowedApp")
-			defines 		{"NDEBUG"}
-			flags 			{"Optimize"}
-			targetdir 		("builds/".._ACTION.."/bin/Release/")
-			
-			if system == "windows" then
-				defines 	{"SFML_STATIC", "SFGUI_STATIC"}
-				links 		{"lua52", "sfgui-s", "thor-s", "sfml-graphics-s", "sfml-window-s", "sfml-system-s"}
-			else 
-				links 		{"lua5.2", "sfgui", "thor", "sfml-graphics", "sfml-window", "sfml-system"}
-			end
+        configuration("Release")
+            kind            ("WindowedApp")
+            defines         {"NDEBUG"}
+            flags           {"Optimize"}
+            targetdir       ("builds/".._ACTION.."/bin/Release/")
+            
+            if system == "windows" then
+                defines     {"SFML_STATIC", "SFGUI_STATIC"}
+                links       {"lua52", "sfgui-s", "thor-s", "sfml-graphics-s", "sfml-window-s", "sfml-system-s"}
+            else 
+                links       {"lua5.2", "sfgui", "thor", "sfml-graphics", "sfml-window", "sfml-system"}
+            end
