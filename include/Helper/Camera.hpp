c39fa2333a7b9aa95e5e08905ace8407783af237
Zephilinox <zephilinox@hotmail.co.uk>
2014-11-03 09:29:32 +0000

    Camera movement & broken camera zoom limits

--- include/Helper/Camera.hpp
+++ include/Helper/Camera.hpp
@@ -16,9 +16,14 @@ private:
     sf::RenderWindow* m_window;
     sf::View m_view;
 
+    float m_zoomLimits[2];
+    float m_zoomValue;
+
     float m_panBorderLimits[4];
     float m_panZonePercent;
     float m_panSpeed;
+
+    float m_moveSpeed;
 
     bool m_updateWindow;
 };
--- src/Helper/Camera.cpp
+++ src/Helper/Camera.cpp
@@ -5,10 +5,15 @@
 Camera::Camera(sf::RenderWindow* window)
     : m_window(window)
     , m_view(m_window->getView())
+    , m_zoomValue(1.f)
     , m_panZonePercent(0.1f)
     , m_panSpeed(4.f)
+    , m_moveSpeed(512.f)
     , m_updateWindow(false)
 {
+    m_zoomLimits[0] = 0.5f;
+    m_zoomLimits[1] = 2.0f;
+
     m_panBorderLimits[0] = m_window->getSize().x * m_panZonePercent;
     m_panBorderLimits[1] = m_window->getSize().x * (1.f-m_panZonePercent);
     m_panBorderLimits[2] = m_window->getSize().y * m_panZonePercent;
@@ -21,8 +26,23 @@ void Camera::handleEvent(const sf::Event& e)
     {
         case sf::Event::MouseWheelMoved:
         {
-            m_view.zoom(1.f + (e.mouseWheel.delta * -0.1)); //TODO: Make Smooth Zoom
-            m_updateWindow = true;
+            float zoomDelta = (e.mouseWheel.delta * -0.1);
+            if (m_zoomValue + zoomDelta >= m_zoomLimits[0] &&
+                m_zoomValue + zoomDelta <= m_zoomLimits[1])
+            {
+                //BUG: Floating point causes error with zooming limits, so if you keep zooming in and out you will be too zoomed in eventually
+                m_zoomValue += zoomDelta;
+                m_view.zoom(1.f + zoomDelta); //TODO: Make Smooth Zoom
+                m_updateWindow = true;
+            }
+            else
+            {
+                auto defView = m_window->getDefaultView();
+            }
+
+            std::cout << m_zoomValue << "\n";
+
+            break;
         }
 
         case sf::Event::Resized:
@@ -31,6 +51,7 @@ void Camera::handleEvent(const sf::Event& e)
             m_panBorderLimits[1] = m_window->getSize().x * (1.f-m_panZonePercent);
             m_panBorderLimits[2] = m_window->getSize().y * m_panZonePercent;
             m_panBorderLimits[3] = m_window->getSize().y * (1.f-m_panZonePercent);
+            break;
         }
 
         default:
@@ -67,6 +88,28 @@ void Camera::update(double dt)
     {
         float deltaY = mousePos.y - m_panBorderLimits[3];
         m_view.move(0, m_panSpeed * deltaY * dt);
+        m_updateWindow = true;
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+    {
+        m_view.move(-m_moveSpeed * dt, 0);
+        m_updateWindow = true;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+    {
+        m_view.move(m_moveSpeed * dt, 0);
+        m_updateWindow = true;
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+    {
+        m_view.move(0, -m_moveSpeed * dt);
+        m_updateWindow = true;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+    {
+        m_view.move(0, m_moveSpeed * dt);
         m_updateWindow = true;
     }
 
