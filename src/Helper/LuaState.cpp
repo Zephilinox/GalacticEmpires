c553abfd76a58129fdf616932f07f67ec2c2f5d9
Zephilinox <zephilinox@hotmail.co.uk>
2014-11-03 16:51:28 +0000

    LuaState throws exceptions in release mode now

--- src/Helper/LuaState.cpp
+++ src/Helper/LuaState.cpp
@@ -36,12 +36,10 @@ int LuaState::loadFile(std::string file)
     lua_pushcfunction(m_luaState, traceback);
     int error = luaL_loadfile(m_luaState, file.c_str());
 
-    #ifdef DEBUG
     if (error)
     {
         throw std::runtime_error("[LuaState::loadFile] " + luaErrorAsString(error) + "\n\n" + lua_tostring(m_luaState, -1));
     }
-    #endif //DEBUG
 
     return error;
 }
@@ -50,13 +48,10 @@ int LuaState::executeFile(std::string file)
 {
     int error = lua_pcall(m_luaState, 0, 0, lua_gettop(m_luaState) - 1);
 
-    #ifdef DEBUG
     if (error)
     {
         throw std::runtime_error("[LuaState::executeFile] " + luaErrorAsString(error) + "\n\n" + lua_tostring(m_luaState, -1));
     }
-
-    #endif //DEBUG
 
     return error;
 }
