828a160066d3a563e697e01971745aec8246c64c
jhpy1024 <jhpy1024@gmail.com>
2014-11-07 19:11:16 +0000

    Removed use of deprecated features.
    Instead of using std::{unary, binary}_function, we now specialize
    std::hash.

--- include/Game/SolarSystem.hpp
+++ include/Game/SolarSystem.hpp
@@ -10,24 +10,19 @@
 
 typedef sf::Vector2i coordinates;
 
-struct key_hash : public std::unary_function<coordinates, std::size_t>
+namespace std
 {
-    std::size_t operator()(const coordinates& coord) const
+    template <>
+    struct hash<coordinates>
     {
-        return coord.x ^ coord.y;
-    }
-};
+        std::size_t operator()(const coordinates& coord) const
+        {
+            return coord.x ^ coord.y;
+        }
+    };
+}
 
-struct key_equal : public std::binary_function<coordinates, coordinates, bool>
-{
-    bool operator()(const coordinates& v0, const coordinates& v1) const
-    {
-        return (v0.x == v1.x &&
-                v0.y == v1.y);
-    }
-};
-
-typedef std::unordered_map<coordinates, sf::CircleShape, key_hash, key_equal> HexMap;
+typedef std::unordered_map<coordinates, sf::CircleShape> HexMap;
 
 class SolarSystem : public sf::Drawable
 {
